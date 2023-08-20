b241251af935896000a4b7aebc6f058820895b90
hokori <hokori.pon@gmail.com>
2023-08-20 14:02:53 +0900

    ✨ アーカイブのスクロールバーをLinkコンポーネントに修正。

--- src/component/Scrollbar.tsx
+++ src/component/Scrollbar.tsx
@@ -6,6 +6,7 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const images = [
   { path: '/archive-vol1.webp', data: '2018.05.03 - 2018.05.04', title: 'ぽんぽこ24' },
@@ -48,24 +49,26 @@ export const Scrollbar = () => {
     >
       {images.map((image) => (
         <SwiperSlide key={image.path}>
-          <div className='grid-cols-1-1 grid-rows-2   rounded-lg p-5  bg-green-800 bg-opacity-10'>
-            <Image
-              className='rounded-lg mb-2'
-              src={image.path}
-              width={200}
-              height={100}
-              sizes='100vw'
-              style={{
-                width: '100%',
-                height: 'auto',
-              }}
-              alt=''
-            />
-            <div>
-              <p className='font-bold text-[15px]'>{image.data}</p>
-              <h2 className='font-bold text-[19px]'>{image.title}</h2>
+          <Link href='#'>
+            <div className='grid-cols-1 grid-rows-2  rounded-lg p-5  bg-green-800 bg-opacity-10 cursor-pointer'>
+              <Image
+                className='rounded-lg mb-2'
+                src={image.path}
+                width={200}
+                height={100}
+                sizes='100vw'
+                style={{
+                  width: '100%',
+                  height: 'auto',
+                }}
+                alt=''
+              />
+              <div>
+                <p className='font-bold text-[15px]'>{image.data}</p>
+                <h2 className='font-bold text-[19px]'>{image.title}</h2>
+              </div>
             </div>
-          </div>
+          </Link>
         </SwiperSlide>
       ))}
     </Swiper>
